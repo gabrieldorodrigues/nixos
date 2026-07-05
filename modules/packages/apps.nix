44d964a95ed9e8e04733a762c33c4d124b9e8d01
gabrieldorodrigues <gabriell.dorodrigues@gmail.com>
2026-07-04 21:18:39 -0300

    fix(readest): disable WebKitGTK DMABUF renderer to fix Wayland crash

--- modules/packages/apps.nix
+++ modules/packages/apps.nix
@@ -4,6 +4,21 @@
 let
   # Zen browser (from flake input).
   zen-browser = inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default;
+
+  # readest is a Tauri/WebKitGTK app. On Wayland its DMABUF renderer triggers
+  # "Error 71 (Protocol error) dispatching to Wayland display" and the app
+  # crashes on startup. Disabling the DMABUF renderer makes WebKitGTK fall back
+  # to a working compositing path. We wrap only readest (via symlinkJoin +
+  # wrapProgram) so no other app is affected. Shadows pkgs.readest below.
+  readest = pkgs.symlinkJoin {
+    name = "readest";
+    paths = [ pkgs.readest ];
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/readest \
+        --set WEBKIT_DISABLE_DMABUF_RENDERER 1
+    '';
+  };
 in
 {
   environment.systemPackages =
